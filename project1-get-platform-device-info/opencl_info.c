51ff468e4446bc0a7a02c0fbbcd404021e60db37
KimJungChul <kjc5443@gmail.com>
2018-08-21 13:10:59 +0900

    update devices info code

--- project1-get-platform-device-info/opencl_info.c
+++ project1-get-platform-device-info/opencl_info.c
@@ -51,7 +51,7 @@ int main() {
   for(idx = 0; idx < num_platforms; idx ++) {
     clGetPlatformInfo(platforms[idx], CL_PLATFORM_NAME, sizeof(char) * PLATFORM_NAME, platform_name, NULL);
     clGetPlatformInfo(platforms[idx], CL_PLATFORM_VENDOR, sizeof(char) * PLATFORM_VENDOR, platform_vendor, NULL);
-    printf("[*] platform : %d\n", idx);
+    printf("[-] platform : %d\n", idx);
     printf("CL_PLATFORM_NAME : %s\n", platform_name);
     printf("CL_PLATFORM_VENDOR : %s\n\n", platform_vendor);
   }
@@ -61,12 +61,18 @@ int main() {
   devices = (cl_device_id *)malloc(sizeof(cl_device_id) * num_devices);
   clGetDeviceIDs(platforms, CL_DEVICE_TYPE_ALL, num_devices, devices, NULL);
   CHECK_ERROR(err);
-  printf("[*] Number of devices : %d\n", num_devices);
+  printf("[-] Number of devices : %d\n", num_devices);
 
   // Get devices info
   for(idx = 0; idx < num_devices; idx ++) {
   	clGetDeviceInfo(devices[idx], CL_DEVICE_TYPE, sizeof(char) * DEVICE_TYPE, device_type, NULL);
   	clGetDeviceInfo(devices[idx], CL_DEVICE_NAME, sizeof(char) * DEVICE_NAME, device_name, NULL);
+  	clGetDeviceInfo(devices[idx], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(cl_uint), &device_max_compute_units, NULL);
+  	clGetDeviceInfo(devices[idx], CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(size_t), &device_max_group_size, NULL);
+  	clGetDeviceInfo(devices[idx], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(cl_ulong), &device_max_group_size, NULL);
+  	clGetDeviceInfo(devices[idx], CL_DEVICE_LOCAL_MEM_SIZE, sizeof(cl_ulong), &device_local_mem_size, NULL);
+  	clGetDeviceInfo(devices[idx], CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(cl_ulong), &device_max_mem_alloc_size, NULL);
+  	clGetDeviceInfo(devices[idx], CL_DEVICE_HOST_UNIFIED_MEMORY, sizeof(cl_ulong), &device_host_unified_memory, NULL);
   	printf("[*] device : %d\n", idx);
   	printf("CL_DEVICE_TYPE : %s\n", device_type);
     printf("CL_DEVICE_NAME : %s\n", device_name);
