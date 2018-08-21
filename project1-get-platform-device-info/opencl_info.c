6c02fe9991db0a4d623acd8e5970cec396dfc327
KimJungChul <kjc5443@gmail.com>
2018-08-21 13:04:02 +0900

    adding get devices info

--- project1-get-platform-device-info/opencl_info.c
+++ project1-get-platform-device-info/opencl_info.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <CL/cl.h>
-#define PLATFORM_NAME 512
-#define PLATFORM_VENDOR 256
+#define PLATFORM_NAME 128
+#define PLATFORM_VENDOR 128
+#define DEVICE_NAME 128
+# define DEVICE_TYPE 128
+
 
 #define CHECK_ERROR(err) \
   if (err != CL_SUCCESS) { \
@@ -16,11 +19,27 @@ int main() {
   cl_platform_id *platforms;
   char platform_name[PLATFORM_NAME];
   char platform_vendor[PLATFORM_VENDOR];
+
+  // device variables
+  cl_uint num_devices;
+  cl_deviced_id *devices;
+  char device_name[DEVICE_NAME];
+  char device_type[DEVICE_NAME];
+  cl_uint device_max_compute_units;
+  size_t device_max_work_group_size;
+  cl_ulong device_global_mem_size;
+  cl_ulong device_local_mem_size;
+  cl_ulong device_max_group_size;
+  cl_ulong device_max_mem_alloc_size;
+  cl_bool device_host_unified_memory;
+
+
+  // error variable
   cl_int err;
 
   // Get number of platforms
   clGetPlatformIDs(0, NULL, &num_platforms);
-  
+  printf("[*] Number of platforms : %d\n", num_platforms);
 
   // Get platforms
   platforms = (cl_platform_id*) malloc(sizeof(cl_platform_id) * num_platforms);
@@ -29,13 +48,34 @@ int main() {
 
   // Get platforms Info
   cl_ushort idx;
-  printf("Number of platforms : %d\n", num_platforms);
   for(idx = 0; idx < num_platforms; idx ++) {
     clGetPlatformInfo(platforms[idx], CL_PLATFORM_NAME, sizeof(char) * PLATFORM_NAME, platform_name, NULL);
     clGetPlatformInfo(platforms[idx], CL_PLATFORM_VENDOR, sizeof(char) * PLATFORM_VENDOR, platform_vendor, NULL);
     printf("[*] platform : %d\n", idx);
     printf("CL_PLATFORM_NAME : %s\n", platform_name);
-    printf("CL_PLATFORM_VENDOR : %s\n", platform_vendor);
+    printf("CL_PLATFORM_VENDOR : %s\n\n", platform_vendor);
+  }
+
+  // Get number of devices
+  clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, NULL, &num_devices);
+  devices = (cl_deviced_id *)malloc(sizeof(cl_deviced_id) * num_devices);
+  clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, num_devices, devices, NULL);
+  CHECK_ERROR(err);
+  printf("[*] Number of devices : %d\n", num_devices);
+
+  // Get devices info
+  for(idx = 0; idx < num_devices; idx ++) {
+  	clGetDeviceInfo(devices[i], CL_DEVICE_TYPE, sizeof(char) * DEVICE_TYPE, device_type, NULL);
+  	clGetDeviceInfo(devices[i], CL_DEVICE_NAME, sizeof(char) * DEVICE_NAME, device_name, NULL);
+  	printf("[*] device : %d\n", idx);
+  	printf("CL_DEVICE_TYPE : %s\n", device_type);
+    printf("CL_DEVICE_NAME : %s\n", device_name);
+    printf("CL_DEVICE_MAX_COMPUTE_UNITS : %d\n", device_max_compute_units);
+    printf("CL_DEVICE_MAX_WORK_GROUP_SIZE : %d\n", device_max_work_group_size);
+    printf("CL_DEVICE_GLOBAL_MEM_SIZE : %l\n", device_max_group_size);
+    printf("CL_DEVICE_LOCAL_MEM_SIZE : %l\n", device_local_mem_size);
+    printf("CL_DEVICE_MAX_MEM_ALLOC_SIZE : %l\n", device_max_mem_alloc_size);
+    printf("CL_DEVICE_HOST_UNIFIED_MEMORY : %l\n\n", device_host_unified_memory);
   }
 
   return 0;
